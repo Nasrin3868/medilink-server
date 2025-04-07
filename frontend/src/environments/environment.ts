81e8f6932c640e3e371f6788e76fb78a2542a662
Nasrin3868 <er.fathimanasrin@gmail.com>
2025-04-07 13:00:02 +0530

    first commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
-export const environment = {
-    api:'http://localhost:3000'
-};
 // export const environment = {
-//     api:'https://medilink.live'
+//     api:'http://localhost:3000'
 // };
+export const environment = {
+    api:'https://medilink.live'
+};
